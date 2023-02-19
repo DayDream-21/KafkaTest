026e96a9ba2e6ca072664cba481db34c4502108b
DayDream-21 <vova13158@gmail.com>
2023-02-19 18:29:36 +0300

    Initial commit

---
+++ src/test/java/com/slavamashkov/kafkatest/KafkaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slavamashkov.kafkatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
